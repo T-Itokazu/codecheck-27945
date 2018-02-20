4836ebafa6ba09f77f908c7ebcb04320f81f77fe
code-project <dev@code-check.io>
2018-02-20 11:17:30 +0000

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+def main(argv):
+    # このコードは引数と標準出力を用いたサンプルコードです。
+    # このコードは好きなように編集・削除してもらって構いません。
+    # ---
+    # This is a sample code to use arguments and outputs.
+    # Edit and remove this code as you like.
+
+    for i in argv:
+        print('Hello ' + str(i) + '!' )
+
+Words = ["World"]#,"織田信長!","codecheck!"]
+main(Words)
